77c36226fab9594f5f0548050519d367601acee7
Djmackool <djmackool@yahoo.com>
2026-04-08 23:22:40 -0400

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
 window.SUPABASE_URL = "https://Retro-PenPals.supabase.co";
-window.SUPABASE_ANON_KEY = "xnizchgsesbdsdcbczkm";
+window.SUPABASE_ANON_KEY = "sb_publishable_cYzumBn8AVx3n-IlGp6IDA_0yNDJoxS";
